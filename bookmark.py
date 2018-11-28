a65c0dc0564372c0062e837b8bc4b8dcd9cf70e5
valyakuttan <valyakuttan@noreply.com>
2018-11-28 18:52:23 +0530

    Clean file structure

--- bookmark.py
+++ bookmark.py
@@ -1,27 +1,39 @@
 # bookmark.py
 from datetime import timedelta, timezone, datetime
 
-
 # Unix Epoch
 Epoch = datetime(1970, 1, 1, tzinfo=timezone.utc)
 
 
 class Bookmark:
-    def __init__(self, title, url, micro_seconds=0):
+    def __init__(self, title, url, tags=None, micro_seconds=0):
         self.url = url
         self.title = title
+        self.tags = tags if tags else []
         self.date_added = Epoch + timedelta(
             microseconds=micro_seconds
         )
 
     @property
+    def tags_string(self):
+        tags = ", ".join(self.tags)
+        return "[ {0} ]".format(tags) if tags else ""
+
+    @property
     def formatted_date(self):
-        return ("{0:%A} {0:%B} {0:%d} {0:%Y}").format(
-            self.date_added
-        )
+        return ("( {0:%A} {0:%B}"
+                " {0:%d} {0:%Y} )").format(self.date_added)
 
     def __str__(self):
-        return "({0.title}, {0.url})".format(self)
+        fs = ("({0.title} "
+              "{0.url} "
+              "{0.tags!s} "
+              "{0.date_added!s})")
+        return fs.format(self)
 
     def __repr__(self):
-        return "({0.title}, {0.url})".format(self)
+        fs = ("({0.title} "
+              "{0.url} "
+              "{0.tags!r} "
+              "{0.date_added!r})")
+        return fs.format(self)
---
+++ jsonbookmarks.py
@@ -0,0 +1,41 @@
+# jsonbookmarks.py
+
+import json
+
+from bookmark import Bookmark
+
+
+def load_bookmarks_from_json(stream):
+    "Load bookmarks from a json stream."
+    data = json.load(stream)
+    return json_to_bookmarks(data)
+
+
+def json_to_bookmarks(cloud):
+    if "_store" in cloud:
+        return map(json_to_bookmark,
+                   cloud["_store"]["store"].values())
+    else:
+        return []
+
+
+def json_to_bookmark(json):
+    return Bookmark(json["_bookmarkTitle"],
+                    json["_bookmarkUrl"],
+                    json["_bookmarkTags"],
+                    json["_bookmarkDateAdded"])
+
+
+if __name__ == "__main__":
+
+    path = "/home/valyakuttan/Downloads/Bookmarks/data/bookmarks"
+    file_name = path + "/bookmark-cloud-0.jsonon"
+
+    with open(file_name) as f:
+        x = json.load(f)
+        bs = json_to_bookmarks(x)
+        tags = [b.tags_string for b in bs]
+
+        print(bs)
+        print()
+        print(tags)
