a7cd0ba83a74989739265e45d885863ecebdeb70
valyakuttan <valyakuttan@noreply.com>
2018-12-02 11:20:05 +0530

    Moved some variables from global scope to local scope.

--- htmltemplate.py
+++ htmltemplate.py
@@ -6,14 +6,14 @@ from jinja2 import Environment, FileSystemLoader
 __all__ = ["html_output"]
 
 
-# Load template file templates/site.html
-TEMPLATE_FILE = "site.html"
-templateLoader = FileSystemLoader(searchpath="templates/")
-templateEnv = Environment(loader=templateLoader)
-template = templateEnv.get_template(TEMPLATE_FILE)
+def html_output(my_list, my_title, my_list_title="",
+                TEMPLATE_FILE="site.html",
+                searchpath="templates/"):
+    TEMPLATE_FILE = TEMPLATE_FILE
+    templateLoader = FileSystemLoader(searchpath="templates/")
+    templateEnv = Environment(loader=templateLoader)
+    template = templateEnv.get_template(TEMPLATE_FILE)
 
-
-def html_output(my_list, my_title, my_list_title=""):
     return template.render(list=my_list,
                            title=my_title,
                            list_title=my_list_title)
